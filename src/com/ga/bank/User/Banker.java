735e4dd73bea294466391deaebce995a25b1643d
akbarhassan <akbar.alsaleh@gmail.com>
2025-12-08 02:28:35 +0300

    add basic functions structure

--- src/com/ga/bank/User/Banker.java
+++ src/com/ga/bank/User/Banker.java
@@ -1,7 +1,25 @@
 package com.ga.bank.User;
 
 public class Banker extends User {
-    public Banker(String userName,String fullName, String email, String password) {
-        super(userName,fullName, email, password, Role.BANKER);
+    public Banker(String userName, String fullName, String email, String password) {
+        super(userName, fullName, email, password, Role.BANKER);
     }
+
+    public void viewUserDetails(String userName) {
+    }
+
+    public void deActivateAccount(String accountId) {
+    }
+
+    public void activateAccount(String accountId) {
+    }
+
+    public void listUserTransactions(String accountId) {
+    }
+
+
+    public void createBankerAccount() {
+    }
+
+
 }
